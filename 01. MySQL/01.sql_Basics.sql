99e2b33f3bb5d9c77e4a6e29784fb9a9c623f6b3
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-03-18 13:58:21 +0200

    Add files via upload

---
+++ 01. MySQL/01.sql_Basics.sql
@@ -0,0 +1,10 @@
+-- 
+create table es (
+First_name varchar(7) ,
+age int ,
+email varchar(15)) ; 
+
+insert into es (first_name,age, email) 
+values('Esraa' , 24, 'esraa@gmail.com');
+
+select * from es ; 
