54add04f379e0300910bd2c40d8b66b7b69d6505
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-03-20 23:43:44 +0200

    Update 004.Data_cleaning.sql

--- 01. MySQL/004.Data_cleaning.sql
+++ 01. MySQL/004.Data_cleaning.sql
@@ -1,1 +1,28 @@
+-- Data Cleaning with MySql
+---------------------------
+
+select * from layoffs ; 
+
+-- 1. Remove Dublicates 
+-- 2. Standarized the Data 
+-- 3. Null values or blank values 
+-- 4. Remove any columns 
+
+
+create table layoffs_staging
+like layoffs ; 
+
+insert layoffs_staging 
+select * 
+from layoffs ; 
+ 
+select * from layoffs_staging;
+------------------------------
+
+-- to kow dublicate roww (if have the sam number ) 
+--------------------------------------------------
+select * ,
+row_number() over(partition by company,industry, 
+total_laid_off, percentage_laid_off, `date` ) as rn
+from  layoffs_staging;
 
