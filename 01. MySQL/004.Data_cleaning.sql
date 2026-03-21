a30391548a2f4afbaa2af1dcb31e1e803b75b2fb
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-03-21 10:11:25 +0200

    Remove dublicates

--- 01. MySQL/004.Data_cleaning.sql
+++ 01. MySQL/004.Data_cleaning.sql
@@ -8,6 +8,9 @@ select * from layoffs ;
 -- 3. Null values or blank values 
 -- 4. Remove any columns 
 
+------------------------
+-- 1. Remove Dublicates 
+------------------------
 
 create table layoffs_staging
 like layoffs ; 
@@ -26,3 +29,70 @@ row_number() over(partition by company,industry,
 total_laid_off, percentage_laid_off, `date` ) as rn
 from  layoffs_staging;
 
+with duplicate_cte as 
+(
+select * ,
+row_number() over(partition by company,industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country,funds_raised_millions ) as rn
+from  layoffs_staging
+)
+select  * 
+from duplicate_cte 
+where rn > 1 ; 
+-------------- 
+-- test to show ; 
+select* 
+from layoffs_staging 
+where company = 'Casper'; 
+-------------------------
+
+with duplicate_cte as 
+(
+select * ,
+row_number() over(partition by company,industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country,funds_raised_millions ) as rn
+from  layoffs_staging
+)
+delete  
+from duplicate_cte 
+where rn > 1 ; 
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL ,
+  `rn` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+select  * 
+from layoffs_staging2 ;
+ 
+ 
+ insert into layoffs_staging2 
+ select * , 
+row_number() over(partition by company,location,industry, 
+total_laid_off, percentage_laid_off, `date`, stage, 
+country,funds_raised_millions ) as rn 
+from  layoffs_staging ;  
+ 
+ -- cuse I'm in save mood 
+SET SQL_SAFE_UPDATES = 0;
+delete
+from layoffs_staging2 
+where rn > 1 ;
+SET SQL_SAFE_UPDATES = 1;
+
+select * 
+from layoffs_staging2 
+where rn > 1 ;
+
+------------------------------
+ 
