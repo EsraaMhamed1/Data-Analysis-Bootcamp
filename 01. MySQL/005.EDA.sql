af6aa7f4e986457d67e30c2309d6f211b5ec3244
Esraa Mhamed El tehawy <109869150+EsraaMhamed1@users.noreply.github.com>
2026-03-23 21:11:49 +0200

    Update 005.EDA.sql

--- 01. MySQL/005.EDA.sql
+++ 01. MySQL/005.EDA.sql
@@ -1,1 +1,51 @@
 
+-- Exploraty Data Analysis 
+---------------------------
+
+select *
+from layoffs_staging2; 
+
+select max(total_laid_off) , max(percentage_laid_off)
+from layoffs_staging2;
+
+select *
+from layoffs_staging2
+where percentage_laid_off = 1
+order by total_laid_off desc;
+
+select company, sum(total_laid_off)
+from layoffs_staging2 
+group by company
+order by 2 desc;
+
+
+select min(`date`), max(`date`)
+from layoffs_staging2 ; 
+
+
+select Industry, sum(total_laid_off)
+from layoffs_staging2 
+group by Industry
+order by 2 desc;
+
+
+select * 
+from layoffs_staging2 ; 
+
+
+select YEAR(`date`), sum(total_laid_off)
+from layoffs_staging2 
+group by year(`date`)
+order by 1 desc;
+
+
+select stage, sum(total_laid_off)
+from layoffs_staging2 
+group by stage
+order by 1 desc;
+
+
+
+
+
+
